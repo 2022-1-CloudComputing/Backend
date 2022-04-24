c803bfa045dcaaf5b54d21f40370e8fb79ec97f1
InKyung H <red131729@khu.ac.kr>
2022-04-24 18:05:31 +0900

    파일 CRUD API 기본 구현
    - boto3를 이용해 CRUD 구현 진행
    - request.FILES 내용에 맞춰 수정해야 함
    - download path 대충 설정함. 수정 필요

--- file/models.py
+++ file/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+
 class User(models.Model):
     name = models.CharField(max_length=100)
 
+
 class File(models.Model):
     title = models.CharField(max_length=200)
+    file = models.FileField(null=True, upload_to="file")
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class Bookmark(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='bookmarks')
-    file = models.ForeignKey(File, on_delete = models.CASCADE, related_name='users')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookmarks")
+    file = models.ForeignKey(File, on_delete=models.CASCADE, related_name="users")
--- file/views.py
+++ file/views.py
@@ -1,14 +1,88 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
 from rest_framework import viewsets
 from rest_framework.response import Response
-from file.models import User, Bookmark
+
+from file.models import Bookmark, File, User
 from file.serializers import BookmarkSerializer
-from django.shortcuts import get_object_or_404
+from file.storages import CRUD
+
 
 class BookmarkViewSet(viewsets.ViewSet):
-    lookup_field = 'userId'
-    
+    lookup_field = "userId"
+
     def list(self, request, userId):
-        user = User.objects.get(id = userId)
+        user = User.objects.get(id=userId)
         queryset = user.bookmarks.all()
         serializer = BookmarkSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class FileUploadView(View):
+    s3_client = CRUD()
+
+    def upload_file(self, request):
+        if request.method == "POST" and len(request.FILES) != 0:
+            file = request.FILES
+            user_id = request.headers["user_id"]
+
+            res = self.s3_client.upload(user_id, file)
+            if res:
+                return JsonResponse({"message": "success upload"})
+            else:
+                return JsonResponse({"message": "fail upload"})
+        else:
+            return JsonResponse({"message": "invalid requests"})
+
+    def delete_file(self, request):
+        if request.method == "DELETE":
+            file = request.FILES
+            user_id = request.headers["user_id"]
+
+            res = self.s3_client.delete(user_id, file)
+            if res:
+                return JsonResponse({"message": "success delete"})
+            else:
+                return JsonResponse({"message": "fail delete"})
+        else:
+            return JsonResponse({"message": "invalid requests"})
+
+    def read_file(self, request):
+        if request.method == "GET":
+            file = request.FILES
+            user_id = request.headers["user_id"]
+
+            res = self.s3_client.read(user_id, file)
+            if res:
+                return JsonResponse({"message": "success read"})
+            else:
+                return JsonResponse({"message": "fail read"})
+        else:
+            return JsonResponse({"message": "invalid requests"})
+
+    def download_file(self, request):
+        if request.method == "GET":
+            file = request.FILES
+            user_id = request.headers["user_id"]
+
+            res = self.s3_client.read(user_id, file)
+            if res:
+                return JsonResponse({"message": "success download"})
+            else:
+                return JsonResponse({"message": "fail download"})
+        else:
+            return JsonResponse({"message": "invalid requests"})
+
+    def update_file(self, request, content):
+        if request.method == "PUT":
+            file = request.FILES
+            user_id = request.headers["user_id"]
+
+            res = self.s3_client.update(user_id, file, content)
+            if res:
+                return JsonResponse({"message": "success update"})
+            else:
+                return JsonResponse({"message": "fail update"})
+        else:
+            return JsonResponse({"message": "invalid requests"})
