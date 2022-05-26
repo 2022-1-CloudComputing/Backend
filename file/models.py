850704d4d8cea6c35cd775f4b6a5f7236563c199
Subin Min <tnqls5417@naver.com>
2022-05-26 19:47:14 +0900

    북마크 모델 수정
    - 추가 구현된 사용자 모델로 연관 변경
    - 추가 구현된 파일 내용 반영

--- file/models.py
+++ file/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from user import models as user_models
+from user.models import User
 
 
 class File(models.Model):
     title = models.CharField(max_length=200, null=True)  # 파일 이름
     file = models.FileField(blank=False, upload_to="media")  # 파일 자체
     file_path = models.CharField(max_length=200, null=False)  # 파일 저장 경로
-    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     created_at = models.DateTimeField(auto_now_add=True)  # 생성 일자
 
     def __str__(self):
@@ -19,5 +19,5 @@ class Folder(models.Model):
     title = models.CharField(max_length=100)
 
 class Bookmark(models.Model):
-    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="bookmarks")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookmarks")
     file = models.ForeignKey(File, on_delete=models.CASCADE, related_name="users")
--- file/serializers.py
+++ file/serializers.py
@@ -2,17 +2,15 @@ from rest_framework import serializers
 
 from file.models import Bookmark, File
 
-
-class BookmarkSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source="file.id")
-    title = serializers.ReadOnlyField(source="file.title")
-
-    class Meta:
-        model = Bookmark
-        fields = ["id", "title"]
-
-
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = "__all__"
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    file = FileSerializer()
+    class Meta:
+        model = Bookmark
+        fields = "__all__"
+        related_object = 'file'
