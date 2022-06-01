fc04bcd487723b7f0c62d3977f8e63bf73ebb202
InKyung H <red131729@khu.ac.kr>
2022-06-01 18:35:37 +0900

    change file models
    - add s3_url and delete file_path

--- file/models.py
+++ file/models.py
@@ -1,21 +1,8 @@
+import time
+
 from botocore import serialize
 from django.db import models
 from user.models import User
-
-
-class File(models.Model):
-    title = models.CharField(max_length=200, null=True)  # 파일 이름
-    file = models.FileField(blank=False, upload_to="media")  # 파일 자체
-    file_path = models.CharField(max_length=200, null=False)  # 파일 저장 경로
-    owner = models.ForeignKey("user.User", on_delete=models.CASCADE, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)  # 생성 일자
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["created_at", "title"]
-
 
 # File 에 외래키로 폴더 지정 필요
 # parent_folder_id= models.ForeignKey('Folder', on_delete=models.CASCADE, null=True)
@@ -37,6 +24,23 @@ class Folder(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class File(models.Model):
+    file_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=True)
+    file = models.FileField(blank=False, upload_to="media")  # 파일 자체
+    title = models.CharField(max_length=200, null=True)  # 파일 이름
+    owner = models.ForeignKey("user.User", on_delete=models.CASCADE, null=False, to_field="username")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)  # 생성 일자
+    file_size = models.PositiveIntegerField(editable=False)  # 파일 크기
+    folder_id = models.ForeignKey("Folder", on_delete=models.CASCADE, null=True, db_column="folder_id")
+    s3_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["created_at", "title"]
 
 
 class Bookmark(models.Model):
