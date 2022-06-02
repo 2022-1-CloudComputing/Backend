38ca6b832727cf1b5bb548efc781856211a77028
Ubuntu <ubuntu@ip-172-31-33-62.ap-northeast-2.compute.internal>
2022-06-02 15:09:51 +0000

    add requirement

--- dropbox/settings.py
+++ dropbox/settings.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = "dropbox.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = my_secrets.DATABASES
+DATABASES = my_secrets.DATABASES
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "mydatabase",}}  # 테스트 위해서 임시로 선언
+#DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "mydatabase",}}  # 테스트 위해서 임시로 선언
 
 
 # Password validation
